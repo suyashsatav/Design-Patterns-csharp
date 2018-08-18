fe66a159716e9c1680108e9bc6681f97239e5871
Suyash Satav <suyash.satav@hotmail.com>
2018-08-18 21:36:41 +0000

    Abstract factory and singleton patterns

--- DP.AbstractFactoryPattern/Program.cs
+++ DP.AbstractFactoryPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DP.AbstractFactoryPattern.Implementations;
+using System;
 
 namespace DP.AbstractFactoryPattern
 {
@@ -6,7 +7,27 @@ namespace DP.AbstractFactoryPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Honda
+            Honda honda = new Honda();
+            VehicleFactory vehicleFactoryHonda = new VehicleFactory(honda);
+
+            Bike hondaBike = vehicleFactoryHonda.GetBike();
+            Console.WriteLine(hondaBike.BikeName);
+
+            Car hondaCar = vehicleFactoryHonda.GetCar();
+            Console.WriteLine(hondaCar.CarName);
+
+            // Mercedes
+            Mercedes mercedes = new Mercedes();
+            VehicleFactory vehicleFactoryMercedes = new VehicleFactory(mercedes);
+
+            Bike MercedesBike = vehicleFactoryMercedes.GetBike();
+            Console.WriteLine(MercedesBike.BikeName);
+
+            Car MercedesCar = vehicleFactoryMercedes.GetCar();
+            Console.WriteLine(MercedesCar.CarName);
+
+            Console.ReadLine();
         }
     }
 }
